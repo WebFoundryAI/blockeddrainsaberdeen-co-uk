f37ad335b74b2798aee70c152656f7f1805bc39e
Steve Wyman <cloudventiresonline@gmail.com>
2026-03-14 11:32:39 +0000

    chore: configure production domain blockeddrainsaberdeen.co.uk

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://BlockedDrainsAberdeen.co.uk/',
+  site: 'https://blockeddrainsaberdeen.co.uk',
   integrations: [
     sitemap(),
     tailwind(),
